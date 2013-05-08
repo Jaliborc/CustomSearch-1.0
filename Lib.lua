3b475edef46a1b2d7bf7b9a11cb1f7165f58e70b
Jaliborc <contact@jaliborc.com>
2013-05-07 23:09:54 -0400

    Renamed "item" variables

--- Lib.lua
+++ Lib.lua
@@ -1,9 +1,21 @@
 --[[
-	ItemSearch
-		An item text search engine of some sort
+Copyright 2013 João Cardoso
+LibPetSearch is distributed under the terms of the GNU General Public License (Version 3).
+As a special exception, the copyright holders of this addon do not give permission to
+redistribute and/or modify it.
+
+This addon is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with the addon. If not, see <http://www.gnu.org/licenses/gpl-3.0.txt>.
+
+This file is part of LibPetSearch.
 --]]
 
-local Lib = LibStub:NewLibrary('LibItemSearch-1.1', 2)
+local Lib = LibStub:NewLibrary('LibPetSearch-1.0', 1)
 if Lib then
 	Lib.filters = {}
 else
@@ -27,20 +39,13 @@ function Lib:Matches(link, search)
 	end
 end
 
-function Lib:InSet(link, search)
-	if IsEquippableItem(link) then
-		local id = tonumber(link:match('item:(%-?%d+)'))
-		return self:BelongsToSet(id, lower(search))
-	end
-end
-
 
 --[[ Parsing ]]--
 
-function Lib:MatchAll(item, ...)
+function Lib:MatchAll(link, ...)
 	for i = 1, select('#', ...) do
 		local search = select(i, ...)
-		if not self:MatchOne(item, strsplit('|', search)) then
+		if not self:MatchOne(link, strsplit('|', search)) then
       		return
 		end
 	end
@@ -48,22 +53,22 @@ function Lib:MatchAll(item, ...)
 	return true
 end
 
-function Lib:MatchOne(item, ...)
+function Lib:MatchOne(link, ...)
 	for i = 1, select('#', ...) do
 		local search = select(i, ...)
-		if useful(search) and self:Match(item, search) then
+		if useful(search) and self:Match(link, search) then
         	return true
 		end
 	end
 end
 
-function Lib:Match(item, search)
+function Lib:Match(link, search)
 	local negated = search:match('^[!~][%s]*(.+)$')
 	if negated then
-		return not self:Filter(item, negated)
+		return not self:Filter(link, negated)
 	end
 
-	return self:Filter(item, search, true)
+	return self:Filter(link, search, true)
 end
 
 
@@ -77,7 +82,7 @@ function Lib:IterateFilters()
 	return pairs(self.filters)
 end
 
-function Lib:Filter(item, search, default)
+function Lib:Filter(link, search, default)
 	local tag, rest = search:match('^[%s]*(%w+):(.*)$')
 	if tag then
 		tag = '^' .. tag
@@ -93,14 +98,14 @@ function Lib:Filter(item, search, default)
 				if filter.tags then
 					for _, value in pairs(filter.tags) do
 						if value:find(tag) then
-							return self:UseFilter(filter, item, operator, search)
+							return self:UseFilter(filter, link, operator, search)
 						end
 					end
 				end
 			end
 		else
 			for id, filter in self:IterateFilters() do
-				if not filter.onlyTags and self:UseFilter(filter, item, operator, search) then
+				if not filter.onlyTags and self:UseFilter(filter, link, operator, search) then
 					return true
 				end
 			end
@@ -112,9 +117,9 @@ function Lib:Filter(item, search, default)
 	return default
 end
 
-function Lib:UseFilter(filter, item, operator, search)
+function Lib:UseFilter(filter, link, operator, search)
 	local capture1, capture2, capture3 = filter:canSearch(operator, search)
 	if capture1 then
-		return filter:findItem(item, operator, capture1, capture2, capture3)
+		return filter:match(link, operator, capture1, capture2, capture3)
 	end
 end

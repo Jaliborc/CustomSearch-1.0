2c080cf3f40f2aa8e8d1a754e4e1336fd33d60c5
Jaliborc <contact@jaliborc.com>
2013-06-22 12:23:03 +0100

    Version bump

--- CustomSearch-1.0.lua
+++ CustomSearch-1.0.lua
@@ -18,7 +18,7 @@ along with the library. If not, see <http://www.gnu.org/licenses/gpl-3.0.txt>.
 This file is part of CustomSearch.
 --]]
 
-local Lib = LibStub:NewLibrary('CustomSearch-1.0', 1)
+local Lib = LibStub:NewLibrary('CustomSearch-1.0', 2)
 if not Lib then
 	return
 end

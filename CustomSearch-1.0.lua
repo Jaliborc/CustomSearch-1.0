8e9a5a3bb3a42da0e3b7354dccc1f73c534f37bb
João Cardoso <contact@jaliborc.com>
2016-05-19 18:26:19 -0700

    Fixes #1

--- CustomSearch-1.0.lua
+++ CustomSearch-1.0.lua
@@ -18,7 +18,7 @@ along with the library. If not, see <http://www.gnu.org/licenses/gpl-3.0.txt>.
 This file is part of CustomSearch.
 --]]
 
-local Lib = LibStub:NewLibrary('CustomSearch-1.0', 7)
+local Lib = LibStub:NewLibrary('CustomSearch-1.0', 8)
 if not Lib then
 	return
 end
@@ -141,6 +141,7 @@ end
 
 function Lib:Clean(string)
 	string = string:lower()
+	string = string:gsub('[%(%)%.%%%+%-%*%?%[%]%^%$]', function(c) return '%'..c end)
 
 	for accent, char in pairs(self.ACCENTS) do
 		string = string:gsub(accent, char)
